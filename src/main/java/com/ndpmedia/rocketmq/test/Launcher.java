7abbc0cb121dc774eea1a02a476609b2723539e9
Li Zhanhui <lizhanhui@gmail.com>
2016-01-21 00:59:09 +0800

    initial commit

---
+++ src/main/java/com/ndpmedia/rocketmq/test/Launcher.java
@@ -0,0 +1,91 @@
+package com.ndpmedia.rocketmq.test;
+
+import com.alibaba.rocketmq.broker.BrokerStartup;
+import com.alibaba.rocketmq.namesrv.NamesrvStartup;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.setProperty("enable_ssl", "true");
+        final String home = System.getProperty("user.home") + File.separator + "rocketmq";
+        System.setProperty("rocketmq.home.dir", home);
+        System.setProperty("log.home", System.getProperty("user.home"));
+
+        checkAndCreateDirs(home);
+        deployConfigResources(home);
+
+        NamesrvStartup.main(args);
+
+        String[] params = {"-c", home + File.separator + "conf" + File.separator + "broker.properties"};
+        BrokerStartup.main(params);
+    }
+
+    private static File checkAndCreateDirs(String path) {
+        File file = new File(path);
+        if (!file.isDirectory() || !file.exists()) {
+            if (!file.mkdirs()) {
+                System.exit(1);
+            }
+        }
+
+        return file;
+    }
+
+    private static File deployConfigResources(String home) {
+        String confDir = home + File.separator + "conf";
+        File confDirFile = checkAndCreateDirs(confDir);
+        deployConfig(confDir, "broker.properties");
+        deployConfig(confDir, "logback_broker.xml");
+        deployConfig(confDir, "logback_namesrv.xml");
+        return confDirFile;
+    }
+
+    private static void deployConfig(String dstFolder, String config) {
+        BufferedOutputStream bos = null;
+        BufferedInputStream bis = null;
+        try {
+            File dstBrokerConfigFile = new File(dstFolder, config);
+
+            if (dstBrokerConfigFile.exists() && dstBrokerConfigFile.isFile()) {
+                return;
+            }
+
+            bos = new BufferedOutputStream(new FileOutputStream(dstBrokerConfigFile, false));
+
+            InputStream inputStream = Launcher.class.getClassLoader().getResourceAsStream(config);
+            bis = new BufferedInputStream(inputStream);
+
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = bis.read(buffer)) > 0) {
+                bos.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != bos) {
+                try {
+                    bos.flush();
+                    bos.close();
+                } catch (IOException e) {
+                    // Ignore
+                }
+            }
+
+            if (null != bis) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    // ignore.
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/com/ndpmedia/rocketmq/test/IntegrationTest.java
@@ -0,0 +1,67 @@
+package com.ndpmedia.rocketmq.test;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.Message;
+import com.alibaba.rocketmq.common.message.MessageExt;
+import junit.framework.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class IntegrationTest {
+
+    @BeforeClass
+    public static void beforeClass() {
+        Launcher.main(null);
+
+        try {
+            Thread.sleep(3 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSendMessage() throws Exception {
+        DefaultMQProducer producer = new DefaultMQProducer("PG_QuickStart");
+        producer.setNamesrvAddr("127.0.0.1:9876");
+        producer.start();
+
+        Message message = new Message("T_QuickStart", "Hello".getBytes());
+        SendResult sendResult = producer.send(message);
+        Assert.assertNotNull(sendResult);
+        producer.shutdown();
+    }
+
+
+    @Test
+    public void testConsumeMessage() throws Exception {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("CG_QuickStart");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        final AtomicInteger count = new AtomicInteger(0);
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("T_QuickStart", null);
+        consumer.setMessageListener(new MessageListenerConcurrently() {
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+                count.addAndGet(msgs.size());
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+
+        consumer.start();
+        Thread.sleep(30 * 1000);
+        Assert.assertTrue(count.get() > 0);
+        consumer.shutdown();
+    }
+
+
+}
